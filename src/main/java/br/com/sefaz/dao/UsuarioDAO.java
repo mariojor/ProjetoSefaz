0eeeebeb32fb755a596c279489dc44ddb17b60be
Albuquerque <m.albuquerque@accenture.com>
2020-10-06 23:17:19 -0300

    Commit inicial
    Entity
    Dao
    package

---
+++ src/main/java/br/com/sefaz/dao/UsuarioDAO.java
@@ -0,0 +1,5 @@
+package br.com.sefaz.dao;
+
+public class UsuarioDAO {
+
+}
---
+++ src/main/java/br/com/sefaz/domain/Usuario.java
@@ -0,0 +1,35 @@
+package br.com.sefaz.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private String senha;
+	
+	@ElementCollection
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet();
+	
+}
